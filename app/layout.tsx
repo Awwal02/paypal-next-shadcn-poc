a3f15e526129b2582337a1b39935d94a1e6c4282
Awwal02 <awwaladarsh@gmail.com>
2024-09-06 14:02:44 -0700

    adding auth0 authentication and mock json server

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import {UserProvider } from '@auth0/nextjs-auth0/client'
 
 export const metadata: Metadata = {
   title: {
@@ -38,20 +39,17 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="relative flex min-h-screen flex-col">
-            <SiteHeader />
-            <div className="flex-1">{children}</div>
-          </div>
-          <TailwindIndicator />
-        </ThemeProvider>
-      </body>
+      <UserProvider>
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+          
+        </body>
+      </UserProvider>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,24 @@
-import Link from "next/link"
+"use client";
+import React from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client'
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-
-export default function IndexPage() {
+function page() {
+  const {user, error, isLoading} = useUser();
+  console.log(user)
+  if(isLoading) return <div>Loading...</div>
+  if(error) return <div>{error?.message}</div>
+  if(user) {
+    return (
+      <div>
+      <a title="test" href="/api/auth/logout">logout</a>
+    </div>
+    )
+  }
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with ShadcnUI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 14 🎉 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
-    </section>
+    <div>
+      <a title="test" href="/api/auth/login">Login</a>
+    </div>
   )
 }
+
+export default page
